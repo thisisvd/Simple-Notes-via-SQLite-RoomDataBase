96a5719d485dd693f119a757bbab85e591fc0deb
Vimal <vimaldubey123@outlook.com>
2021-03-22 01:59:44 +0530

    more work final for today

--- app/src/main/java/com/vdcodeassociate/simple_notes/LinedEditText.java
+++ app/src/main/java/com/vdcodeassociate/simple_notes/LinedEditText.java
@@ -14,32 +14,42 @@ import androidx.appcompat.widget.AppCompatEditText;
 
 public class LinedEditText extends AppCompatEditText {
 
+    private static final String TAG = "LinedEditText";
+
     private Rect mRect;
     private Paint mPaint;
 
-    public LinedEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
+
+    // we need this constructor for LayoutInflater
+    public LinedEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+
         mRect = new Rect();
         mPaint = new Paint();
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(2);
-        mPaint.setColor(0xFFFFFFFF);
+        mPaint.setColor(0xFFFFD966); // Color of the lines on paper
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
+        // get the height of the view
         int height = ((View)this.getParent()).getHeight();
+
         int lineHeight = getLineHeight();
-        int numberOfLines = height/lineHeight;
+        int numberOfLines = height / lineHeight;
 
         Rect r = mRect;
         Paint paint = mPaint;
 
-        int baseline = getLineBounds(0,r);
+        int baseline = getLineBounds(0, r);
 
-        for(int i=0;i<numberOfLines;i++){
-            canvas.drawLine(r.left,baseline+1,r.right,baseline+1,paint);
+        for (int i = 0; i < numberOfLines; i++) {
+
+            canvas.drawLine(r.left, baseline + 1, r.right, baseline + 1, paint);
+
             baseline += lineHeight;
         }
 
